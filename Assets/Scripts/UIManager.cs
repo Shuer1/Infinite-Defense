abeed3371eaf7351706af63b04ac3c90148c5441
Shuer1 <cujitom@gmail.com>
2025-09-05 11:23:51 +0800

    "Add essential Scripts"

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    //单例模式
+    private static UIManager _instance;
+    public static UIManager Instance { get { return _instance; } }
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
