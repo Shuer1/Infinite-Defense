42f06c0cedb0999b32756f74f1ee10b4a928374e
Shuer1 <cujitom@gmail.com>
2025-09-07 20:41:21 +0800

    "Test commit"

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target;       // 玩家
+    public Transform target;       // 玩家Target
     public Vector3 offset = new Vector3(0, 15, -10); // 相机相对位置
     public float smoothSpeed = 5f; // 平滑跟随速度
 
