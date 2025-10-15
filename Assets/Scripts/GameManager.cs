8dd4784127b12ed63c6cf78aac360d1c8fdfb27a
Shuer1 <cujitom@gmail.com>
2025-10-15 15:51:01 +0800

    sth fixed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -33,11 +33,6 @@ public class GameManager : MonoBehaviour
 
     public void GameOver()
     {
-        /*
-        if (isGameOver) return;
-
-        isGameOver = true;
-        */
         DataManager.FlushSave();
         UIManager.Instance.ShowGameOver();
     }
