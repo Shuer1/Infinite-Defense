d6238ac7162d1f16cf548dd045f4cdb98ea756f1
Shuer1 <cujitom@gmail.com>
2025-11-07 15:57:09 +0800

    新怪物类型添加测试成功！需完善：新怪物生成逻辑

--- Assets/Scripts/GameDataInitialConfig.cs
+++ Assets/Scripts/GameDataInitialConfig.cs
@@ -38,21 +38,21 @@ public static class EnemysInitialConfig
 {
     public const int EnemiesLevel = 1;
     //敌人1
-    public const int Enemy1MaxHealth = 100;
+    public const int Enemy1MaxHealth = 150;
     public const int Enemy1Damage = 20;
     public const int Enemy1ExpReward = 10;
     //敌人2
-    public const int Enemy2MaxHealth = 150;
+    public const int Enemy2MaxHealth = 200;
     public const int Enemy2Damage = 35;
     public const int Enemy2ExpReward = 25;
     //怪物1
-    public const int Monster1MaxHealth = 200;
+    public const int Monster1MaxHealth = 2000;
     public const int Monster1Damage = 50;
     public const int Monster1ExpReward = 50;
     //怪物2
-    public const int Monster2MaxHealth = 500;
+    public const int Monster2MaxHealth = 2800;
     public const int Monster2Damage = 75;
-    public const int Monster2ExpReward = 150;
+    public const int Monster2ExpReward = 100;
 
 }
 
